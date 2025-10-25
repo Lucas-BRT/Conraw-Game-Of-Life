81f7d0bb3943a2ea503d17a66949856fee6227f1
lucasbrt <lucasbritodepaula.t@gmail.com>
2025-10-25 08:29:42 -0300

    feat: implementa versão funcional do mundo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use rand::Rng;
 use std::{io::stdin, thread, time::Duration, usize};
 
-const WORLD_SIZE: usize = 30;
-const FRAME_TIME: usize = 10;
+const WORLD_SIZE: usize = 20;
+const INITIAL_POPULATION: u64 = 200;
+const FRAME_TIME: usize = 600;
 
 #[derive(Debug, Clone, Copy)]
 struct Cell {
@@ -24,6 +25,37 @@ impl Cell {
             position: Position { x, y },
         }
     }
+
+    fn kill(&mut self) {
+        self.alive = false;
+    }
+
+    fn count_neighbor(&self, world: &World) -> u8 {
+        let mut counter = 0;
+
+        for dy in -1..=1 {
+            for dx in -1..=1 {
+                if dy == 0 && dx == 0 {
+                    continue;
+                }
+
+                let x = self.position.x as i32 + dx;
+                let y = self.position.y as i32 + dy;
+
+                if x >= 0 && y >= 0 {
+                    if let Some(row) = world.space.get(y as usize) {
+                        if let Some(cell) = row.get(x as usize) {
+                            if cell.alive {
+                                counter += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return counter;
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -33,26 +65,27 @@ struct Position {
 }
 
 struct World {
+    start: bool,
     population: u64,
-    position: Position,
+    size: Position,
     space: Vec<Vec<Cell>>,
 }
 
 impl World {
-    fn new(position: Position) -> Self {
+    fn new(size: Position) -> Self {
         let mut space = Vec::new();
 
-        for y in 0..position.y {
+        for y in 0..size.y {
             space.push(Vec::new());
-
-            for x in 0..position.x {
+            for x in 0..size.x {
                 space[y].push(Cell::new(x, y));
             }
         }
 
         return Self {
+            start: true,
             population: 0,
-            position,
+            size,
             space,
         };
     }
@@ -64,8 +97,8 @@ impl World {
 
     fn randon_populate_world(&mut self, mut population: u32) {
         while population > 0 {
-            let rand_x_poss = rand::rng().random_range(0..self.position.x);
-            let rand_y_poss = rand::rng().random_range(0..self.position.y);
+            let rand_x_poss = rand::rng().random_range(0..self.size.x);
+            let rand_y_poss = rand::rng().random_range(0..self.size.y);
             let current_cell = &mut self.space[rand_y_poss][rand_x_poss];
             if !current_cell.alive {
                 current_cell.alive = true;
@@ -73,10 +106,40 @@ impl World {
             }
         }
     }
+
+    fn tick(&mut self) {
+        let mut space = self.space.clone();
+
+        if !self.start && self.population == 0 {
+            return;
+        }
+
+        for row in 0..self.size.y {
+            for column in 0..self.size.x {
+                let a = &mut space[row][column];
+                let neighbor = a.count_neighbor(self);
+                if a.alive {
+                    if neighbor < 2 {
+                        a.kill();
+                    }
+
+                    if neighbor > 3 {
+                        a.kill();
+                    }
+                } else {
+                    if neighbor == 3 {
+                        a.alive = true;
+                    }
+                }
+            }
+        }
+
+        self.space = space;
+    }
 }
 
 trait WorldDriver {
-    fn render(&self, world: &World);
+    fn render(&self, world: &mut World);
     fn handle_input(&mut self, word: &mut World);
     fn sleep(&self);
 }
@@ -105,12 +168,12 @@ impl TerminalDriver {
 }
 
 impl WorldDriver for TerminalDriver {
-    fn render(&self, world: &World) {
+    fn render(&self, world: &mut World) {
         TerminalDriver::clean_terminal();
-        TerminalDriver::show_horizontal_separator(world.position.x);
-        for y in 0..world.position.y {
+        TerminalDriver::show_horizontal_separator(world.size.x);
+        for y in 0..world.size.y {
             print!("|");
-            for x in 0..world.position.x {
+            for x in 0..world.size.x {
                 match world.space[y][x].alive {
                     true => print!("O"),
                     false => print!(" "),
@@ -119,7 +182,8 @@ impl WorldDriver for TerminalDriver {
             println!("|");
         }
 
-        TerminalDriver::show_horizontal_separator(world.position.x);
+        TerminalDriver::show_horizontal_separator(world.size.x);
+        world.tick();
     }
 
     fn sleep(&self) {
@@ -174,13 +238,50 @@ fn main() {
         y: WORLD_SIZE,
     });
 
-    world.randon_populate_world(20);
+    world.randon_populate_world(INITIAL_POPULATION as u32);
 
-    let mut terminal_driver = TerminalDriver::new();
+    let terminal_driver = TerminalDriver::new();
 
     loop {
         terminal_driver.render(&mut world);
-        terminal_driver.handle_input(&mut world);
+        // terminal_driver.handle_input(&mut world);
         terminal_driver.sleep();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn count_neighbors() {
+        let mut world = World::new(Position {
+            x: WORLD_SIZE,
+            y: WORLD_SIZE,
+        });
+
+        let first_cell = Cell::new_alive(0, 0);
+        world.add_cell(first_cell);
+        world.add_cell(Cell::new_alive(0, 1));
+        world.add_cell(Cell::new_alive(1, 0));
+        world.add_cell(Cell::new_alive(1, 1));
+
+        assert_eq!(first_cell.count_neighbor(&world), 3);
+    }
+
+    #[test]
+    fn count_up_neighbors() {
+        let mut world = World::new(Position {
+            x: WORLD_SIZE,
+            y: WORLD_SIZE,
+        });
+
+        let first_cell = Cell::new_alive(1, 1);
+        world.add_cell(first_cell);
+        world.add_cell(Cell::new_alive(0, 1));
+        world.add_cell(Cell::new_alive(0, 1));
+        world.add_cell(Cell::new_alive(0, 1));
+
+        assert_eq!(first_cell.count_neighbor(&world), 3);
     }
 }
